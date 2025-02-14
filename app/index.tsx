c2471484cf9a2effdd5574812cffc3fdd75f1c88
matheusmneves <matheusneves.m@gmail.com>
2025-02-13 21:04:31 -0700

    clerk auth signin

--- app/index.tsx
+++ app/index.tsx
@@ -12,28 +12,26 @@ export default function SignIn(){
 
   const [isLoading, setIsLoading] = useState(false)
 
-  const googleOAuth = useSSO()
+  const { startSSOFlow } = useSSO()
 
   async function onGoogleSignIn(){
     try {
       setIsLoading(true)
       console.log("setisloading")
 
-      const oAuthFlow = await googleOAuth.startSSOFlow({
-        strategy: 'oauth_google'
+      const { createdSessionId, setActive, signIn, signUp } = await startSSOFlow({
+        strategy: 'oauth_google',
       })
 
-      if(oAuthFlow.authSessionResult?.type === 'success'){
-        console.log("success condition")
-        if(oAuthFlow.setActive){
-          await oAuthFlow.setActive({
-            session: oAuthFlow.createdSessionId,
-          })
-        } else {
-          setIsLoading(false)
-        }
-        router.replace('/')
+      console.log(signIn?.status)
+      console.log(signUp?.status)
+      if (createdSessionId) {
+        setActive!({ session: createdSessionId })
+        console.log(createdSessionId)
+      } else {
+        setIsLoading(false)
       }
+      router.replace('/')
     } catch (error) {
       console.log(error)
     }
@@ -74,3 +72,59 @@ title: {
   fontWeight: 'bold',
 }
 })
+
+
+
+// export default function SignIn(){
+//   console.log("signin function")
+
+// const [isLoading, setIsLoading] = useState(false)
+
+// const googleOAuth = useSSO()
+
+// async function onGoogleSignIn(){
+//   try {
+//     setIsLoading(true)
+//     console.log("setisloading")
+
+//     const oAuthFlow = await googleOAuth.startSSOFlow({
+//       strategy: 'oauth_google'
+//     })
+
+//     if(oAuthFlow.authSessionResult?.type === 'success'){
+//       console.log("success condition")
+//       if(oAuthFlow.setActive){
+//         await oAuthFlow.setActive({
+//           session: oAuthFlow.createdSessionId,
+//         })
+//       } else {
+//         setIsLoading(false)
+//       }
+//       router.replace('/')
+//     }
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+
+// useEffect(() => {
+//   WebBrowser.warmUpAsync()
+
+//   return () => {
+//     WebBrowser.coolDownAsync()
+
+//   }
+// },[])
+
+// return (
+// <View style={styles.container}>
+//   <Text style={styles.title}>Sign in</Text>
+//   <SButton 
+//     icon="logo-google" 
+//     title='Sign in with Google'
+//     onPress={onGoogleSignIn}
+//     isLoading={isLoading}
+//   />
+// </View>
+// )
+// }
