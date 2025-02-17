f9e73359b2a21459b61a2a224289592361c9c68e
Bruno Accorsi Bergoli <b.bergoli@gmail.com>
2025-02-17 10:40:24 -0700

    add: implement authentication layout and Google SSO flow

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,36 +1,74 @@
-import { View, Text, StyleSheet, Image } from "react-native";
-import { SButton } from "../components/Button";
-import { useAuth, useUser } from "@clerk/clerk-expo"
+import React, { useCallback, useEffect } from 'react';
+import * as WebBrowser from 'expo-web-browser';
+import { useSSO } from '@clerk/clerk-expo';
+import { View, Button, StyleSheet } from 'react-native';
+import { SButton } from '../components/Button';
 
-export default function Home(){
-    const { user } = useUser()
-    const { signOut } = useAuth()
+export const useWarmUpBrowser = () => {
+  useEffect(() => {
+    // Preloads the browser for Android devices to reduce authentication load time
+    // See: https://docs.expo.dev/guides/authentication/#improving-user-experience
+    void WebBrowser.warmUpAsync();
+    return () => {
+      // Cleanup: closes browser when component unmounts
+      void WebBrowser.coolDownAsync();
+    };
+  }, []);
+};
 
-    return (
-        <View style={styles.container}>
-            <Image source={{ uri: user?.imageUrl }} style={styles.image}/>
-            <Text style={styles.text}>{user?.fullName}</Text>
-            <SButton icon="exit" title="Exit" onPress={() => signOut()}/>
-        </View>
-    )
+// Handle any pending authentication sessions
+WebBrowser.maybeCompleteAuthSession();
+
+export default function Page() {
+  useWarmUpBrowser();
+
+  // Use the `useSSO()` hook to access the `startSSOFlow()` method
+  const { startSSOFlow } = useSSO();
+
+  const onPress = useCallback(async () => {
+    try {
+      // Start the authentication process by calling `startSSOFlow()`
+      const { createdSessionId, setActive, signIn, signUp } =
+        await startSSOFlow({
+          strategy: 'oauth_google',
+        });
+
+      // If sign in was successful, set the active session
+      if (createdSessionId) {
+        setActive!({ session: createdSessionId });
+      } else {
+        // If there is no `createdSessionId`,
+        // there are missing requirements, such as MFA
+        // Use the `signIn` or `signUp` returned from `startSSOFlow`
+        // to handle next steps
+      }
+    } catch (err) {
+      // See https://clerk.com/docs/custom-flows/error-handling
+      // for more info on error handling
+      console.error(JSON.stringify(err, null, 2));
+    }
+  }, [startSSOFlow]);
+
+  return (
+    <View style={styles.container}>
+      <SButton
+        icon="logo-google"
+        title="Sign in with Google"
+        onPress={onPress}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 32,
-        justifyContent: "center",
-        alignItems: "center",
-        gap: 12,
-    },
-    text: {
-        fontSize: 18,
-        fontWeight: "bold",
-    },
-    image: {
-        width: 92,
-        height: 92,
-        borderRadius: 12
-    }
-
-})
+  container: {
+    flex: 1,
+    padding: 32,
+    justifyContent: 'center',
+    gap: 12,
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,72 +1,17 @@
-import { Slot, router } from 'expo-router';
-import { ClerkProvider, useAuth } from '@clerk/clerk-expo';
-import { useEffect } from 'react'
-import { ActivityIndicator } from 'react-native'
 import { tokenCache } from '@/storage/tokenCache';
+import { ClerkProvider } from '@clerk/clerk-expo';
+import { Slot } from 'expo-router';
 
-const PUBLIC_CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY as string
-
-function InitialLayout(){
-  const { isSignedIn, isLoaded } = useAuth()
-
-  useEffect(() => {
-    if(!isLoaded) return
-
-    if(isSignedIn){
-      router.replace("/(auth)")
-    } else {
-      router.replace("/(public)")
-    }
-  }, [isSignedIn])
-  
-  return isLoaded ? ( 
-    <Slot />
-    ) : (
-      <ActivityIndicator style={{ flex: 1, justifyContent: "center", alignItems: "center"}}
-      />
-    )
-}
+const PUBLIC_CLERK_PUBLISHABLE_KEY = process.env
+  .EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY as string;
 
 export default function Layout() {
   return (
-    <ClerkProvider 
-    publishableKey={PUBLIC_CLERK_PUBLISHABLE_KEY} 
-    tokenCache={tokenCache}
+    <ClerkProvider
+      tokenCache={tokenCache}
+      publishableKey={PUBLIC_CLERK_PUBLISHABLE_KEY}
     >
-      
       <Slot />
     </ClerkProvider>
-  )
+  );
 }
-
-
-
-// import React from 'react';
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import { Slot, Redirect } from 'expo-router';
-// import { PaperProvider } from 'react-native-paper';
-// import { customTheme } from './theme';
-// import { ClerkProvider, ClerkLoaded, useAuth } from '@clerk/clerk-expo';
-// import { tokenCache } from '@/cache';
-
-// const queryClient = new QueryClient();
-// const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
-
-// if (!publishableKey) {
-//   throw new Error('Missing EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY');
-// }
-
-// export default function RootLayout() {
-//   return (
-//     <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
-//       <ClerkLoaded>
-//         <PaperProvider theme={customTheme}>
-//           <QueryClientProvider client={queryClient}>
-//             {/* Se o usuário estiver logado, <Slot /> renderiza o restante do app */}
-//             <Slot />
-//           </QueryClientProvider>
-//         </PaperProvider>
-//       </ClerkLoaded>
-//     </ClerkProvider>
-//   );
-// }
